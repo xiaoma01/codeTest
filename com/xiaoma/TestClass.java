0a6a41263f9998ec2e00f9b730d75c9414e5a60c
xiaoma01 <877692886@qq.com>
2019-07-27 21:53:10 +0800

    测试能否提交成功

---
+++ com/xiaoma/TestClass.java
@@ -0,0 +1,10 @@
+package com.xiaoma;
+
+/**
+ * Created by Administrator on 2019/7/27.
+ */
+public class TestClass {
+    public static void main(String[] args){
+        System.out.println("测试通过git提交github 的项目");
+    }
+}
